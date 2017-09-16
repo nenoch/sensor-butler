4fd65c1c7e7a5d1e96a6ee34d27b55183beaed8d
Irene Canuti <irene.canuti@gmail.com>
2017-09-16 14:50:08 +0100

    Add try/except block to prevent the request if RPi not connected to WiFi

--- pir.py
+++ pir.py
@@ -1,12 +1,18 @@
 from gpiozero import MotionSensor
 import json
 import requests
+import urllib2
 
 pir = MotionSensor(4)
 webhook_url = 'https://hooks.slack.com/services/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'
 json_text = {'text':'Door :door: is open :open_hands:!'}
 
 while True:
+    try:
+        urllib2.urlopen("http://www.google.com")
+    except urllib2.URLError:
+        print('Not Connected yet')
+    else:
     if pir.motion_detected:
         print('motion detected...')
         response = requests.post(
